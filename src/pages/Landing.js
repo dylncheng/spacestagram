f213363f6608a92c08042ee1d4844cbd1ded0a61
Dylan Cheng <chengdylan02@gmail.com>
2022-01-13 23:23:36 -0800

    added post limiting and 'show more' feauture

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,32 +2,44 @@ import { useEffect, useState } from "react";
 import Loading from "../loading/Loading";
 import Post from "../components/Post";
 import "./Landing.css";
+import Contact from "../components/Contact";
 
 /*API KEY NOT INCLUDED IN REPO*/
-const API_KEY = "################";
+const API_KEY = "Je7NMXeDDyeMHqkZkI7XmiRHgcGjFiHUFwy0PYVf";
 
 
 const Landing = () => {
     const [photos, setPhotos] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [linkCopied, setLinkCopied] = useState(false);
-    const [i, setI] = useState(0);
+    const [maxPhotos, setMaxPhotos] = useState(10);
+
+    const handleShowMore = () => {
+        setMaxPhotos((prevMaxPhotos) => prevMaxPhotos + 10);
+    };
 
     
     useEffect(() => {
         const fetchData = async () => {
-            setIsLoading(true)
-            const res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${API_KEY}`);
-            const data = await res.json()
-            setPhotos(data.photos);
-            setIsLoading(false);
+            setIsLoading(true);
+            let num = Math.floor(Math.random() * 1000);
+            const res = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${num}&api_key=${API_KEY}`);
+            const data = await res.json();
+            
+            if(data.photos.length === 0) {
+                fetchData();  //recursive 
+            } else {
+                setPhotos(data.photos);
+                setIsLoading(false);
+            }
+
+            // alternative 
+            // setIsLoading(false); 
         
         }
-
         fetchData();
-        setI((prevI)=>prevI + 1)
 
-    }, [setIsLoading, setI])
+    }, [setIsLoading]);
 
     return(
         <div className="landing">
@@ -35,8 +47,10 @@ const Landing = () => {
             {!isLoading && <h1 className="logo"> Spacestagram</h1>}
             {linkCopied && <div className="copied-modal"><h3>copied to clipboard</h3></div>}
             <div className="posts">
-                {!isLoading && i < 1000 && photos.map((photo, index) => <Post photo={photo.img_src} name={photo.rover.name} id={index} key={photo.id} date={photo.earth_date} camera={photo.camera.full_name} setLinkCopied={setLinkCopied} linkCopied={linkCopied}/>)}
+                {!isLoading && photos.map((photo, index) => index < maxPhotos?<Post photo={photo.img_src} name={photo.rover.name} id={index} key={photo.id} date={photo.earth_date} camera={photo.camera.full_name} setLinkCopied={setLinkCopied} linkCopied={linkCopied}/>:null)}
             </div>
+            {!isLoading && !(maxPhotos >= photos.length) && <button className="show-more-btn" onClick={handleShowMore}>show more</button>}
+            {!isLoading && <Contact />}
         </div>
     );
 }
